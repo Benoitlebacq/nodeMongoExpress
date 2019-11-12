905620756b387bf9d276a10660f799aa97bfcfb7
Benoit Lebacq <lebacqbenoit@gmail.com>
2019-11-12 15:28:54 +0100

    added colors.js so that the server logs are easier to see

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const colors = require("colors");
+const connectDB = require("./config/db");
+
 //load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// connect to Database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 const app = express();
 
 // dev logging middleware
@@ -17,7 +23,17 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.magenta
+      .italic
+  )
 );
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error : ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
