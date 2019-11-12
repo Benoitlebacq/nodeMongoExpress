553daa938632641fc97925dc2af3df6e88cd51c6
Benoit Lebacq <lebacqbenoit@gmail.com>
2019-11-12 14:10:31 +0100

    removed custom middleware and added morgan middleware

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const logger = require("./middleware/logger");
+const morgan = require("morgan");
 // Route files
 const bootcamps = require("./routes/bootcamps");
 //load env vars
@@ -8,7 +8,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.use(logger);
+// dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
