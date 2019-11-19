094cc72faa308d3319c0e34de2a9efc1db2e2553
Benoit Lebacq <lebacqbenoit@gmail.com>
2019-11-19 15:19:20 +0100

    set the env https

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,13 +18,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role
   });
 
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({
-    success: true,
-    token
-  });
+  sendTokenResponse(user, 400, res);
 });
 
 // @desc        login User
@@ -53,25 +47,24 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse("invalid credentials", 401));
   }
 
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({
-    success: true,
-    token
-  });
+  sendTokenResponse(user, 400, res);
 });
 
 // Get token from model, create cookie and send response
-const sendTokenresponse = (user, statusCode, res) => {
+const sendTokenResponse = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken();
 
   const options = {
     expires: new Date(
       Date.now() + process.env_JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    )
+    ),
+    httpOnly: true
   };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
 
   res
     .status(statusCode)
